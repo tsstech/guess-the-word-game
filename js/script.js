ccd8dd6b13ecdfb94fad34aee82aee62a5706b3c
tsstech <vidisha156@yahoo.com>
2024-04-07 19:06:40 +0100

    Created global variables to track document elements & word variable

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Variables selecting elements from index.html
+const guessedLetters = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const guessInput = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingGuesses = document.querySelector(".remaining span");
+const message = document.querySelector(".message");
+const playAgain = document.querySelector(".play-again");
+
+// Global variable holding the word
+const word = "magnolia";
+
